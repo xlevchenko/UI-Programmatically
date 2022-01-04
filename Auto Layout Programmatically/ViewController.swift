4730ea525789536bfd989b881a6c4488d43c4b84
Olexsii <realcarter50@gmail.com>
2022-01-04 17:30:22 +0200

    added UIPageControl and button

--- Auto Layout Programmatically/ViewController.swift
+++ Auto Layout Programmatically/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 import Foundation
 
+extension UIColor {
+    static var mainPink = UIColor(red: 77/255, green: 94/255, blue: 172/255, alpha: 1)
+}
+
 class ViewController: UIViewController {
 
     let bearImageView: UIImageView = {
@@ -39,6 +43,35 @@ class ViewController: UIViewController {
         return textView
     }()
     
+    //make sure you correct encapsulation practicals in your classes
+    private let privButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.mainPink, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+        pc.currentPageIndicatorTintColor = .mainPink
+        pc.pageIndicatorTintColor = .gray
+        pc.translatesAutoresizingMaskIntoConstraints = false
+        return pc
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -46,8 +79,27 @@ class ViewController: UIViewController {
         view.addSubview(descriptionTextView)
         
         setupLayout()
+        setupBottomControls()
+    }
+    
+    
+    
+    fileprivate func setupBottomControls() {
+        
+        let buttomControlsStackView = UIStackView(arrangedSubviews: [privButton, pageControl, nextButton])
+        buttomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        buttomControlsStackView.distribution = .fillEqually
+        view.addSubview(buttomControlsStackView)
+        
+        NSLayoutConstraint.activate([
+            buttomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            buttomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            buttomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            buttomControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 
+    
     private func setupLayout() {
         
         let topImageContainerView = UIView()
