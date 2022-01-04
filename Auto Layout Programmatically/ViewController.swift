f47120864f59764db4595e1d55a2ab6f4a508a10
Olexsii <realcarter50@gmail.com>
2022-01-04 14:05:14 +0200

    landscape mode

--- Auto Layout Programmatically/ViewController.swift
+++ Auto Layout Programmatically/ViewController.swift
@@ -10,18 +10,28 @@ import Foundation
 
 class ViewController: UIViewController {
 
-    let bearimageView: UIImageView = {
+    let bearImageView: UIImageView = {
         let image = UIImage(named: "bear")
         let imageView = UIImageView(image: image)
+        
         //this enables autolayout for our bearimageView
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
     let descriptionTextView: UITextView = {
         let textView = UITextView()
-        textView.text = "Join us today in our fun and games!"
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        let atributedText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
+        atributedText.append(NSAttributedString(
+            string: "\n\n\n Are you ready for loads and loads of fun? D'ont wait longer! We hope to see you in our store soon", attributes: [
+                NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13),
+                NSAttributedString.Key.foregroundColor: UIColor.gray]))
+        
+        textView.attributedText = atributedText
+        
+
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.textAlignment = .center
         textView.isEditable = false
@@ -31,23 +41,34 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // hear's our entry point into our app
         
-        view.addSubview(bearimageView)
+        // hear's our entry point into our app
         view.addSubview(descriptionTextView)
         
         setupLayout()
     }
 
     private func setupLayout() {
-        bearimageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        bearimageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        bearimageView.widthAnchor.constraint(equalToConstant: 250).isActive = true
-        bearimageView.heightAnchor.constraint(equalToConstant: 250).isActive = true
         
-        descriptionTextView.topAnchor.constraint(equalTo: bearimageView.bottomAnchor, constant: 80).isActive = true
-        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        let topImageContainerView = UIView()
+        view.addSubview(topImageContainerView)
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //enable auto layout
+        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        
+        topImageContainerView.addSubview(bearImageView)
+        bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive  = true
+        bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.6).isActive = true
+        
+        
+        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
+        descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24).isActive = true
+        descriptionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24).isActive = true
         descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
         
     }
